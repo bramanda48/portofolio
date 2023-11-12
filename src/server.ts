95bc3e97d7fc09dd4e4b63e6eac90e6077da096d
Bramanda Suwandi <bramanda48@gmail.com>
2023-11-12 21:34:50 +0700

    feat: change polka to express

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,13 @@ import compression from 'compression';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 const app = express();
+const path= process.cwd();
 
-app.use(express.static(__dirname + '/static'),
+app.use(express.static(path + '/static'),
 		compression({ threshold: 0 }),
 		sapper.middleware()); 
 
 app.listen(PORT, () => {
-	console.info(`We are up! on ${dev}`);
+	console.info(`We are up! on ${NODE_ENV} mode`);
+	console.info(`Project location: ${path}`);
 });
